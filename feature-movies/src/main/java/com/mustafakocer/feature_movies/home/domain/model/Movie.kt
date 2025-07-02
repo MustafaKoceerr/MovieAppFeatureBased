31441f40a07249beedd0d76c24a5c3aa2952be7f
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-07-02 16:17:46 +0300

    Update feature-movies UI components

---
+++ feature-movies/src/main/java/com/mustafakocer/feature_movies/home/domain/model/Movie.kt
@@ -0,0 +1,12 @@
+package com.mustafakocer.feature_movies.home.domain.model
+
+data class Movie(
+    val id: Int,
+    val title: String,
+    val overview: String,
+    val posterPath: String?,
+    val backdropPath: String?,
+    val releaseDate: String,
+    val voteAverage: Double,
+    val genreIds: List<Int>,
+)
