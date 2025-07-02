6db30e0a1e1942ebb3fb16349f67c172fb5b8b6f
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-07-02 16:08:37 +0300

    Update core-ui theme components

---
+++ core-ui/src/main/java/com/mustafakocer/core_ui/ui/theme/Color.kt
@@ -0,0 +1,84 @@
+package com.mustafakocer.core_ui.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = androidx.compose.ui.graphics.Color(0xFFD0BCFF)
+val PurpleGrey80 = androidx.compose.ui.graphics.Color(0xFFCCC2DC)
+val Pink80 = androidx.compose.ui.graphics.Color(0xFFEFB8C8)
+
+val Purple40 = androidx.compose.ui.graphics.Color(0xFF6650a4)
+val PurpleGrey40 = androidx.compose.ui.graphics.Color(0xFF625b71)
+val Pink40 = androidx.compose.ui.graphics.Color(0xFF7D5260)
+
+// Light Theme Colors
+// Primary
+val LightPrimary = androidx.compose.ui.graphics.Color(0xFF2C3E50)
+val LightOnPrimary = androidx.compose.ui.graphics.Color(0xFFFFFFFF)
+val LightPrimaryContainer = androidx.compose.ui.graphics.Color(0xFF7EA6E0)
+val LightOnPrimaryContainer = androidx.compose.ui.graphics.Color(0xFF0A1929)
+
+// Secondary
+val LightSecondary = androidx.compose.ui.graphics.Color(0xFFE74C3C)
+val LightOnSecondary = androidx.compose.ui.graphics.Color(0xFFFFFFFF)
+val LightSecondaryContainer = androidx.compose.ui.graphics.Color(0xFFFFDAD4)
+val LightOnSecondaryContainer = androidx.compose.ui.graphics.Color(0xFF410001)
+
+// Tertiary
+val LightTertiary = androidx.compose.ui.graphics.Color(0xFFF39C12)
+val LightOnTertiary = androidx.compose.ui.graphics.Color(0xFFFFFFFF)
+val LightTertiaryContainer = androidx.compose.ui.graphics.Color(0xFFFFE0B2)
+val LightOnTertiaryContainer = androidx.compose.ui.graphics.Color(0xFF3E2E00)
+
+// Error
+val LightError = androidx.compose.ui.graphics.Color(0xFFFF5252)
+val LightOnError = androidx.compose.ui.graphics.Color(0xFFFFFFFF)
+val LightErrorContainer = androidx.compose.ui.graphics.Color(0xFFFFCDD2)
+val LightOnErrorContainer = androidx.compose.ui.graphics.Color(0xFF410002)
+
+// Neutral
+val LightBackground = androidx.compose.ui.graphics.Color(0xFFF5F5F5)
+val LightOnBackground = androidx.compose.ui.graphics.Color(0xFF1C1B1F)
+val LightSurface = androidx.compose.ui.graphics.Color(0xFFFFFFFF)
+val LightOnSurface = androidx.compose.ui.graphics.Color(0xFF1C1B1F)
+
+// Neutral Variant
+val LightSurfaceVariant = androidx.compose.ui.graphics.Color(0xFFE7E0EC)
+val LightOnSurfaceVariant = androidx.compose.ui.graphics.Color(0xFF49454F)
+val LightOutline = androidx.compose.ui.graphics.Color(0xFF79747E)
+
+// Dark Theme Colors
+// Primary
+val DarkPrimary = androidx.compose.ui.graphics.Color(0xFF93B5DF)
+val DarkOnPrimary = androidx.compose.ui.graphics.Color(0xFF0B2641)
+val DarkPrimaryContainer = androidx.compose.ui.graphics.Color(0xFF1E4D78)
+val DarkOnPrimaryContainer = androidx.compose.ui.graphics.Color(0xFFD6E3FF)
+
+// Secondary
+val DarkSecondary = androidx.compose.ui.graphics.Color(0xFFFF8A80)
+val DarkOnSecondary = androidx.compose.ui.graphics.Color(0xFF680000)
+val DarkSecondaryContainer = androidx.compose.ui.graphics.Color(0xFFAC2F24)
+val DarkOnSecondaryContainer = androidx.compose.ui.graphics.Color(0xFFFFDAD4)
+
+// Tertiary
+val DarkTertiary = androidx.compose.ui.graphics.Color(0xFFFFCC80)
+val DarkOnTertiary = androidx.compose.ui.graphics.Color(0xFF5D3F00)
+val DarkTertiaryContainer = androidx.compose.ui.graphics.Color(0xFFB37300)
+val DarkOnTertiaryContainer = androidx.compose.ui.graphics.Color(0xFFFFE0B2)
+
+// Error
+val DarkError = androidx.compose.ui.graphics.Color(0xFFFF8A80)
+val DarkOnError = androidx.compose.ui.graphics.Color(0xFF680000)
+val DarkErrorContainer = androidx.compose.ui.graphics.Color(0xFFB00020)
+val DarkOnErrorContainer = androidx.compose.ui.graphics.Color(0xFFFFCDD2)
+
+// Neutral
+val DarkBackground = androidx.compose.ui.graphics.Color(0xFF121212)
+val DarkOnBackground = androidx.compose.ui.graphics.Color(0xFFE3E3E3)
+val DarkSurface = androidx.compose.ui.graphics.Color(0xFF1E1E1E)
+val DarkOnSurface = androidx.compose.ui.graphics.Color(0xFFE3E3E3)
+
+// Neutral Variant
+val DarkSurfaceVariant = androidx.compose.ui.graphics.Color(0xFF2F2F2F)
+val DarkOnSurfaceVariant = androidx.compose.ui.graphics.Color(0xFFCACACA)
+val DarkOutline = androidx.compose.ui.graphics.Color(0xFF8E8E8E)
+
