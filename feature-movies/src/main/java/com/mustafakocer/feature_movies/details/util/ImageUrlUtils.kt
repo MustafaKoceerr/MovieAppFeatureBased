152d70617754f9e6482232dca4d98876a89e9d21
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-07-03 10:22:41 +0300

    Add image URL utility functions

---
+++ feature-movies/src/main/java/com/mustafakocer/feature_movies/details/util/ImageUrlUtils.kt
@@ -0,0 +1,20 @@
+package com.mustafakocer.feature_movies.details.util
+
+import com.mustafakocer.feature_movies.MovieConstants
+import com.mustafakocer.feature_movies.details.domain.model.MovieDetails
+
+fun MovieDetails.fullPosterUrl(): String {
+    return if (!posterPath.isNullOrBlank()) {
+        "${MovieConstants.IMAGE_BASE_URL}${MovieConstants.POSTER_SIZE}$posterPath"
+    } else {
+        ""
+    }
+}
+
+fun MovieDetails.fullBackdropUrl(): String {
+    return if (!backdropPath.isNullOrBlank()) {
+        "${MovieConstants.IMAGE_BASE_URL}${MovieConstants.BACKDROP_SIZE}$backdropPath"
+    } else {
+        ""
+    }
+}
