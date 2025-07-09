264d422efc71ceec04a2a3b7de46ae56b041b83b
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-07-09 17:21:57 +0300

    Create individual item for search results

--- feature-movies/src/main/java/com/mustafakocer/feature_movies/search/presentation/components/SearchMovieItem.kt
+++ feature-movies/src/main/java/com/mustafakocer/feature_movies/search/presentation/components/SearchMovieItem.kt
@@ -20,7 +20,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
-import com.mustafakocer.feature_movies.MovieConstants
+import com.mustafakocer.feature_movies.shared.util.MovieConstants
 import com.mustafakocer.feature_movies.shared.domain.model.MovieList
 
 /**
