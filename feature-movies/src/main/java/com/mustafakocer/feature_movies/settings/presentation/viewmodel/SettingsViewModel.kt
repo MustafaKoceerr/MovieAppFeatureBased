5f81cea450f325bbc600951eda369375ff8f5f95
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-07-10 14:59:49 +0300

    Add ViewModel for Settings feature

--- feature-movies/src/main/java/com/mustafakocer/feature_movies/settings/presentation/viewmodel/SettingsViewModel.kt
+++ feature-movies/src/main/java/com/mustafakocer/feature_movies/settings/presentation/viewmodel/SettingsViewModel.kt
@@ -1,4 +1,4 @@
-package com.mustafakocer.feature_movies.settings
+package com.mustafakocer.feature_movies.settings.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
