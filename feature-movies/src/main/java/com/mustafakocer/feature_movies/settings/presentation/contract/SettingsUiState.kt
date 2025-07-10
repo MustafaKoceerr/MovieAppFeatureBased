5812bcdfa62ed9df6b389319ce050ef57fed7a7c
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-07-10 14:59:40 +0300

    Add UI state model for settings

--- feature-movies/src/main/java/com/mustafakocer/feature_movies/settings/presentation/contract/SettingsUiState.kt
+++ feature-movies/src/main/java/com/mustafakocer/feature_movies/settings/presentation/contract/SettingsUiState.kt
@@ -1,4 +1,4 @@
-package com.mustafakocer.feature_movies.settings.contract
+package com.mustafakocer.feature_movies.settings.presentation.contract
 
 import com.mustafakocer.core_common.presentation.BaseUiState
 import com.mustafakocer.core_preferences.models.ThemePreference
