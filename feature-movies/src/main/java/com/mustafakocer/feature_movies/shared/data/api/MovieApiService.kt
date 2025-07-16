31ada30a51b3d0b1e7fe5ebf82fac32375b7b089
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-07-17 02:48:41 +0300

    feat(api): inject language query parameter in all movie API requests

--- feature-movies/src/main/java/com/mustafakocer/feature_movies/shared/data/api/MovieApiService.kt
+++ feature-movies/src/main/java/com/mustafakocer/feature_movies/shared/data/api/MovieApiService.kt
@@ -19,7 +19,6 @@ interface MovieApiService {
     @GET("movie/now_playing")
     suspend fun getNowPlayingMovies(
         @Query("page") page: Int = 1,
-        @Query("language") language: String = "en-US",
     ): Response<PaginatedResponseDto<MovieDto>>
 
     /**
@@ -28,7 +27,6 @@ interface MovieApiService {
     @GET("movie/popular")
     suspend fun getPopularMovies(
         @Query("page") page: Int = 1,
-        @Query("language") language: String = "en-US",
     ): Response<PaginatedResponseDto<MovieDto>>
 
     /**
@@ -37,7 +35,6 @@ interface MovieApiService {
     @GET("movie/top_rated")
     suspend fun getTopRatedMovies(
         @Query("page") page: Int = 1,
-        @Query("language") language: String = "en-US",
     ): Response<PaginatedResponseDto<MovieDto>>
 
     /**
@@ -46,7 +43,6 @@ interface MovieApiService {
     @GET("movie/upcoming")
     suspend fun getUpcomingMovies(
         @Query("page") page: Int = 1,
-        @Query("language") language: String = "en-US",
     ): Response<PaginatedResponseDto<MovieDto>>
 
     /**
@@ -56,7 +52,6 @@ interface MovieApiService {
     suspend fun getMoviesByCategory(
         @Path("category") category: String,
         @Query("page") page: Int = 1,
-        @Query("language") language: String = "en-US",
     ): Response<PaginatedResponseDto<MovieDto>>
 
 
@@ -70,7 +65,6 @@ interface MovieApiService {
     suspend fun searchMovies(
         @Query("query") query: String,
         @Query("page") page: Int = 1,
-        @Query("language") language: String = "en-US",
     ): Response<PaginatedResponseDto<MovieDto>>
 
 
@@ -83,6 +77,5 @@ interface MovieApiService {
     @GET("movie/{movie_id}")
     suspend fun getMovieDetails(
         @Path("movie_id") movieId: Int,
-        @Query("language") language: String = "en-US",
     ): Response<MovieDetailsDto>
 }
