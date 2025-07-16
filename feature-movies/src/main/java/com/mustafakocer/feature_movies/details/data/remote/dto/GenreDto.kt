7072e9408a5b566aeef22a4a1d9b3ee5ad33c24c
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-07-16 12:44:44 +0300

    Reorganized feature modules, updated dependency structure, and improved package organization to align with clean architecture principles.

--- feature-movies/src/main/java/com/mustafakocer/feature_movies/details/data/remote/dto/GenreDto.kt
+++
@@ -1,9 +0,0 @@
-package com.mustafakocer.feature_movies.details.data.remote.dto
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class GenreDto(
-    val id: Int,
-    val name: String
-)
--- feature-movies/src/main/java/com/mustafakocer/feature_movies/settings/presentation/contract/SettingsEffect.kt
+++
@@ -1,9 +0,0 @@
-package com.mustafakocer.feature_movies.settings.presentation.contract
-
-import com.mustafakocer.core_common.presentation.BaseUiEffect
-
-sealed interface SettingsEffect : BaseUiEffect {
-    object NavigateBack : SettingsEffect
-    data class ShowSuccessSnackbar(val message: String) : SettingsEffect
-    data class ShowErrorSnackbar(val message: String) : SettingsEffect
-}
