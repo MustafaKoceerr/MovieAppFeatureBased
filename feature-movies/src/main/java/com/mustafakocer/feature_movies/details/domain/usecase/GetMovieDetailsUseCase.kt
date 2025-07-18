0feb55266ca9b4adc94ef904b0683dc28cec0c7a
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-07-18 17:02:11 +0300

    refactor(usecase): adapt GetMovieDetailsUseCase to new error handling

--- feature-movies/src/main/java/com/mustafakocer/feature_movies/details/domain/usecase/GetMovieDetailsUseCase.kt
+++ feature-movies/src/main/java/com/mustafakocer/feature_movies/details/domain/usecase/GetMovieDetailsUseCase.kt
@@ -1,21 +1,15 @@
 package com.mustafakocer.feature_movies.details.domain.usecase
 
+import com.mustafakocer.core_common.util.Resource
 import com.mustafakocer.feature_movies.details.domain.repository.MovieDetailsRepository
 import com.mustafakocer.feature_movies.shared.domain.model.MovieDetails
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-/**
- * Use case for getting movie details
- *
- * CLEAN ARCHITECTURE: Domain Layer
- * RESPONSIBILITY: Coordinate movie details retrieval business logic
- */
-
 class GetMovieDetailsUseCase @Inject constructor(
     private val repository: MovieDetailsRepository,
 ) {
-    operator fun invoke(movieId: Int): Flow<MovieDetails> {
+    operator fun invoke(movieId: Int): Flow<Resource<MovieDetails>> {
         // BUSINESS RULE: Movie ID must be positive
         require(movieId > 0) { "Movie ID must be positive, but was: $movieId" }
 
