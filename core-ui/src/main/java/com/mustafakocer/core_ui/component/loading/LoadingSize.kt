fc1e05dc591c6d2018dfeaf4573f12232fe42fa5
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-07-18 15:39:14 +0300

    remove deprecated UI error mappers

--- core-ui/src/main/java/com/mustafakocer/core_ui/component/loading/LoadingSize.kt
+++
@@ -1,8 +0,0 @@
-package com.mustafakocer.core_ui.component.loading
-
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
-
-enum class LoadingSize(val size: Dp) {
-    Small(24.dp), Medium(32.dp), Large(48.dp)
-}
