de35c050ded1362874c7c98460acffd7e6f84535
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-07-18 19:10:12 +0300

    refactor(repository): update MovieRepository for safe API call integration

--- feature-movies/src/main/java/com/mustafakocer/feature_movies/home/domain/repository/MovieRepository.kt
+++ feature-movies/src/main/java/com/mustafakocer/feature_movies/home/domain/repository/MovieRepository.kt
@@ -1,18 +1,14 @@
 package com.mustafakocer.feature_movies.home.domain.repository
 
+import com.mustafakocer.core_common.util.Resource
 import com.mustafakocer.feature_movies.shared.domain.model.MovieListItem
 import kotlinx.coroutines.flow.Flow
 
 interface MovieRepository {
 
     // ==================== MOVIE CATEGORIES ====================
-
-    fun getNowPlayingMovies(page: Int = 1): Flow<List<MovieListItem>>
-
-    fun getPopularMovies(page: Int = 1): Flow<List<MovieListItem>>
-
-    fun getTopRatedMovies(page: Int = 1): Flow<List<MovieListItem>>
-
-    fun getUpcomingMovies(page: Int = 1): Flow<List<MovieListItem>>
-
+    fun getNowPlayingMovies(page: Int = 1): Flow<Resource<List<MovieListItem>>>
+    fun getPopularMovies(page: Int = 1): Flow<Resource<List<MovieListItem>>>
+    fun getTopRatedMovies(page: Int = 1): Flow<Resource<List<MovieListItem>>>
+    fun getUpcomingMovies(page: Int = 1): Flow<Resource<List<MovieListItem>>>
 }
