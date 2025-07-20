65c9bc05ddd07a939929dc66579c281a4dd218c8
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-07-20 16:13:18 +0300

    Move language change business logic to GetHomeScreenDataUseCase.

--- feature-movies/src/main/java/com/mustafakocer/feature_movies/list/domain/usecase/GetMovieListUseCase.kt
+++ feature-movies/src/main/java/com/mustafakocer/feature_movies/list/domain/usecase/GetMovieListUseCase.kt
@@ -1,22 +1,37 @@
 package com.mustafakocer.feature_movies.list.domain.usecase
 
 import androidx.paging.PagingData
+import com.mustafakocer.core_preferences.repository.LanguageRepository
 import com.mustafakocer.feature_movies.shared.domain.model.MovieCategory
 import com.mustafakocer.feature_movies.list.domain.repository.MovieListRepository
 import com.mustafakocer.feature_movies.shared.domain.model.MovieListItem
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flatMapLatest
 import javax.inject.Inject
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class GetMovieListUseCase @Inject constructor(
     private val repository: MovieListRepository,
+    private val languageRepository: LanguageRepository,
 ) {
     /**
-     * Get paginated movies for specific category
+     * Belirtilen kategori için PagingData akışını döndürür.
+     * Bu akış, dil her değiştiğinde otomatik olarak güncellenir.
      *
-     * @param category Movie category to fetch
-     * @return Flow of PagingData for reactive pagination
+     * @param category Film listesinin alınacağı kategori.
+     * @return Dil değişikliklerine duyarlı PagingData akışı.
      */
-    operator fun invoke(category: MovieCategory, language: String): Flow<PagingData<MovieListItem>> {
-        return repository.getMoviesByCategory(category, language)
+    operator fun invoke(category: MovieCategory): Flow<PagingData<MovieListItem>> {
+        // 1. dil akışını dinle.
+        return languageRepository.languageFlow
+            .flatMapLatest { language ->
+                // 2. dil her değiştiğinde, repository'den o dile ait paging data akışını al.
+                // flatmapLatest, önceki dil için olan akışı otomatik olarak iptal eder.
+                repository.getMoviesByCategory(
+                    category = category,
+                    language = language.apiParam
+                )
+            }
     }
 }
