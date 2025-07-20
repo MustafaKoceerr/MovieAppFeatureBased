00027efaa975fd982f389b324499063b6daa095c
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-07-21 01:35:55 +0300

    Define auth navigation destinations and actions

---
+++ navigation-contracts/src/main/java/com/mustafakocer/navigation_contracts/navigation/FeatureAuthDestinations.kt
@@ -0,0 +1,12 @@
+package com.mustafakocer.navigation_contracts.navigation
+
+import kotlinx.serialization.Serializable
+
+// :feature-movies modülüne ait tüm hedefler
+
+@Serializable
+object AuthFeatureGraph // Bu, tüm feature'ın kök rotası olacak
+
+@Serializable
+object WelcomeScreen
+
