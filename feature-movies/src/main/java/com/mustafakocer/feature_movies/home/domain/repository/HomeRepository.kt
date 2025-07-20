aa44b775403fd2773ca00c9a97ef196a77b4c85a
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-07-20 15:20:07 +0300

    Update HomeRepository interface to support category-based movie retrieval. Aligned repository methods with the new MovieCategory-driven design.

--- feature-movies/src/main/java/com/mustafakocer/feature_movies/home/domain/repository/HomeRepository.kt
+++ feature-movies/src/main/java/com/mustafakocer/feature_movies/home/domain/repository/HomeRepository.kt
@@ -1,14 +1,10 @@
 package com.mustafakocer.feature_movies.home.domain.repository
 
 import com.mustafakocer.core_common.util.Resource
+import com.mustafakocer.feature_movies.shared.domain.model.MovieCategory
 import com.mustafakocer.feature_movies.shared.domain.model.MovieListItem
 import kotlinx.coroutines.flow.Flow
 
 interface HomeRepository {
-
-    // ==================== MOVIE CATEGORIES ====================
-    fun getNowPlayingMovies(page: Int = 1): Flow<Resource<List<MovieListItem>>>
-    fun getPopularMovies(page: Int = 1): Flow<Resource<List<MovieListItem>>>
-    fun getTopRatedMovies(page: Int = 1): Flow<Resource<List<MovieListItem>>>
-    fun getUpcomingMovies(page: Int = 1): Flow<Resource<List<MovieListItem>>>
+    fun getMoviesForCategory(category: MovieCategory): Flow<Resource<List<MovieListItem>>>
 }
