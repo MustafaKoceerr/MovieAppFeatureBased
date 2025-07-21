457f07f3dd23212e00c7e341852a2d7a968a72cb
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-07-21 20:04:08 +0300

    Add resource helper extension func.

---
+++ core-domain/src/main/java/com/mustafakocer/core_domain/util/ResourceExtensions.kt
@@ -0,0 +1,21 @@
+package com.mustafakocer.core_domain.util
+
+/**
+ * Bir Resource<T> nesnesinin içindeki başarılı veriyi (T) başka bir tipe (R)
+ * dönüştürmek için kullanılır. Hata ve Yükleme durumlarını korur.
+ *
+ * Bu, Repository katmanlarında DTO'dan Domain modeline dönüşüm yaparken
+ * 'when' bloğu tekrarını önler.
+ *
+ * @param T Orijinal veri tipi.
+ * @param R Dönüştürülecek veri tipi.
+ * @param transform Başarılı veri üzerinde uygulanacak dönüşüm fonksiyonu.
+ * @return Dönüştürülmüş veriyi içeren yeni bir Resource<R> nesnesi.
+ */
+fun <T, R> Resource<T>.map(transform: (T) -> R): Resource<R> {
+    return when (this) {
+        is Resource.Success -> Resource.Success(transform(data))
+        is Resource.Error -> this
+        is Resource.Loading -> Resource.Loading
+    }
+}
