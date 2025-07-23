5512caa4d67828a7c21a7a69d91fd8bc60ebf6cb
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-07-23 19:22:35 +0300

    Add Docs to core-android

--- core-android/src/main/java/com/mustafakocer/core_android/util/LocaleUtils.kt
+++ core-android/src/main/java/com/mustafakocer/core_android/util/LocaleUtils.kt
@@ -4,6 +4,16 @@ import android.content.Context
 import android.content.ContextWrapper
 import java.util.Locale
 
+/**
+ * Creates a new Context with an updated Locale configuration.
+ *
+ * This utility is essential for dynamically changing the application's language at runtime.
+ * It generates a new context with the specified locale, which can then be used to inflate
+ * views and access resources in the chosen language.
+ *
+ * @param locale The new [Locale] to be set.
+ * @return A [ContextWrapper] containing the new configuration.
+ */
 fun Context.updateLocale(locale: Locale): ContextWrapper {
     val configuration = this.resources.configuration
     configuration.setLocale(locale)
